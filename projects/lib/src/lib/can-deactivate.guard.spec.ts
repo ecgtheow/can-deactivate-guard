b380c6f486e3c65ef53bcbef8103e1d733beaedc
Dick Porter <dick@ecgtheow.com>
2018-11-18 16:48:16 +0000

    Update tests

--- projects/lib/src/lib/can-deactivate.guard.spec.ts
+++ projects/lib/src/lib/can-deactivate.guard.spec.ts
@@ -1,12 +1,70 @@
 import { TestBed } from '@angular/core/testing';
+import { Observable, of } from 'rxjs';
 
-import { CanDeactivateGuard } from './can-deactivate.guard';
+import { CanDeactivateGuard, CanComponentDeactivate } from './can-deactivate.guard';
+
+class CanComponentDeactivateNotImplemented {
+}
+
+class CanComponentDeactivateImplemented implements CanComponentDeactivate {
+  constructor (private candeactivate: Observable<boolean> | Promise<boolean> | boolean) {}
+
+  canDeactivate (): Observable<boolean> | Promise<boolean> | boolean {
+    return this.candeactivate;
+  }
+}
 
 describe('CanDeactivateGuard', () => {
+  let service: CanDeactivateGuard;
+
   beforeEach(() => TestBed.configureTestingModule({}));
 
+  beforeEach(() => {
+    service = TestBed.get(CanDeactivateGuard);
+  });
+
   it('should be created', () => {
-    const service: CanDeactivateGuard = TestBed.get(CanDeactivateGuard);
     expect(service).toBeTruthy();
+  });
+
+  describe('Component does not implement CanComponentDeactivate', () => {
+    let component: any;
+
+    beforeEach(() => {
+      component = new CanComponentDeactivateNotImplemented();
+    });
+
+    it('should return true if component does not implement CanComponentDeactivate', () => {
+      expect(service.canDeactivate(component as any as CanComponentDeactivate)).toBeTruthy();
+    });
+  });
+
+  describe('Component does implement CanComponentDeactivate', () => {
+    describe('canDeactivate() returns boolean', () => {
+      it('should return the value of component.canDeactivate()', () => {
+        const booleanRet = false;
+        const component = new CanComponentDeactivateImplemented(booleanRet);
+
+        expect(service.canDeactivate(component)).toBe(booleanRet);
+      });
+    });
+
+    describe('canDeactivate() returns Promise<boolean>', () => {
+      it('should return the value of component.canDeactivate()', () => {
+        const booleanPromiseRet: Promise<boolean> = Promise.resolve(false);
+        const component = new CanComponentDeactivateImplemented(booleanPromiseRet);
+
+        expect(service.canDeactivate(component)).toBe(booleanPromiseRet);
+      });
+    });
+
+    describe('canDeactivate() returns Observable<boolean>', () => {
+      it('should return the value of component.canDeactivate()', () => {
+        const booleanObservableRet: Observable<boolean> = of(false);
+        const component = new CanComponentDeactivateImplemented(booleanObservableRet);
+
+        expect(service.canDeactivate(component)).toBe(booleanObservableRet);
+      });
+    });
   });
 });
